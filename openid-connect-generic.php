370a0ae6db675d6a067843318de2a9116e6f1291
Christer Aarø <christeraaro@gmail.com>
2024-04-22 14:49:33 +0200

    Fake version **TEMP FIX** to prevent update

--- openid-connect-generic.php
+++ openid-connect-generic.php
@@ -16,7 +16,7 @@
  * Plugin Name:       OpenID Connect Generic
  * Plugin URI:        https://github.com/daggerhart/openid-connect-generic
  * Description:       Connect to an OpenID Connect identity provider using Authorization Code Flow.
- * Version:           3.9.1
+ * Version:           9.9.1
  * Requires at least: 5.0
  * Requires PHP:      7.4
  * Author:            daggerhart
